b3f04665b5b024d2f70e6393884a2c37711dc37c
Davben93 <davide.benedetto93@gmail.com>
2022-07-04 18:03:39 +0200

    updated livy client manager with kerb confs

--- src/main/java/com/example/livy/client/LivyClientManager.java
+++ src/main/java/com/example/livy/client/LivyClientManager.java
@@ -27,22 +27,42 @@ public class LivyClientManager {
 	}
 	
 	public LivyClient getLivyClient() throws Exception {
-		if(this.livyClient == null) {
-			
+		if (this.livyClient == null) {
+//			String clouderaClientId = LivyConfigurationManager.getInstance().getProperty("livy.client.auth.id");
+//			String clouderaClientSecret = LivyConfigurationManager.getInstance().getProperty("livy.client.auth.secret");
 			Map<String, String> config = new HashMap<>();
-			config.put("spark.app.name", "livy");
-			config.put("livy.client.http.connection.timeout", "180s");
-			config.put("spark.driver.memory", "1g");
+//			config.put("spark.app.name", "livy");
+//			config.put("livy.client.http.connection.timeout", "180s");
+//			config.put("spark.driver.memory", "1g");
 			config.put("spark.sql.crossJoin.enabled", "true");
+//			config.put("client.auth.id", clouderaClientId);
+//			config.put("client.auth.secret", clouderaClientSecret);
+			
+			String javaSecurityAuthLoginConfig = LivyConfigurationManager.getInstance().getProperty("java.security.auth.login.config");
+			System.setProperty("java.security.auth.login.config", javaSecurityAuthLoginConfig);
+			
+			String javaSecurityKrb5Conf = LivyConfigurationManager.getInstance().getProperty("java.security.krb5.conf");
+			System.setProperty("java.security.krb5.conf", javaSecurityKrb5Conf);
+			
+			String sunSecurityKrb5Debug = LivyConfigurationManager.getInstance().getProperty("sun.security.krb5.debug");
+			System.setProperty("sun.security.krb5.debug", sunSecurityKrb5Debug);
+			
+			String javaSecurityAuthUseSubjectCredsOnly = LivyConfigurationManager.getInstance().getProperty("javax.security.auth.useSubjectCredsOnly");
+			System.setProperty("javax.security.auth.useSubjectCredsOnly", javaSecurityAuthUseSubjectCredsOnly);
+			
+			System.out.println("System Properties:"+System.getProperties());
+
 //			config.put("spark.jars", "/home/davben/.livy-sessions/*");
 			String uri = LivyConfigurationManager.getInstance().getProperty("livy.uri");
-			livyClient = new LivyClientBuilder(true).setAll(config).setURI(new URI(uri)).build();
+			livyClient = new LivyClientBuilder(false).setAll(config).setURI(new URI(uri)).build();
 			System.out.println("Uploading jars to the SparkContext...");
-			LivyClientManager.getInstance().getLivyClient().uploadJar(new File("jars/myJarLight.jar")).get();
+			File jarFile = new File("myJarLight.jar");
+			livyClient.uploadJar(jarFile).get();
 			System.out.println("finish uploading jars to the SparkContext...");
 		}
 		return livyClient;
 	}
+	
 
 	public void reset() {
 		instance = null;
